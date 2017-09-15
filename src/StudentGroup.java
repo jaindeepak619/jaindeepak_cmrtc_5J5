ce46d865e7c895323fbb6517264163cc2c21f13b
jaindeepak619 <31984093+jaindeepak619@users.noreply.github.com>
2017-09-15 15:44:43 +0530

    random

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -97,13 +97,7 @@ public class StudentGroup implements StudentArrayOperation {
 	public void addLast(Student student) {
 		// Add your implementation here
 		
-		if(student==null){
-
-			throw new IllegalArgumentException("invalid");
-		}else{
-
-			this.students[this.students.length-1]=student;
-		}
+		
 	}
 
 	@Override
@@ -111,19 +105,7 @@ public class StudentGroup implements StudentArrayOperation {
 		// Add your implementation here
 		
 		
-		if(student==null || (index <0 || index > this.students.length)){
 
-			throw new IllegalArgumentException("invalid");
-		}else{
-
-			for(int i=0;i<this.students.length;i++){
-				
-				if(index==i){
-					
-					this.students[i]=student;
-				}
-			}
-		}
 
 	}
 
